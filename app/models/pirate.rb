776b016de2731e3c0bfaa1df818900d66aad9a07
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-13 13:11:09 -0500

    commit tree 0afdc6a7df772e6c717f3a7b1b6b2cc7aeceb027

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize (args_hash)
+        @name = args_hash["name"]
+        @weight = args_hash["weight"]
+        @height = args_hash["height"]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
