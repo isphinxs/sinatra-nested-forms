0a1199bbee39e4031a48113a980ac00d0b5f5206
Samantha Ostrowski <isphinxs@hotmail.com>
2021-02-13 13:11:16 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all = []
+
+    def initialize(args_hash)
+        @name = args_hash["name"]
+        @type = args_hash["type"]
+        @booty = args_hash["booty"]
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.clear
+        self.all.clear
+    end
 end
